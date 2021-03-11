2691aaa361d5aefcaae1b22db30bde6de28d3aa5
AleksKuwshinov <kuv_aleks@mail.ru>
2021-03-11 18:34:32 +0400

    no commit

--- app/src/main/java/com/aleksey_kuvshinov/calculator/MainActivity.java
+++ app/src/main/java/com/aleksey_kuvshinov/calculator/MainActivity.java
@@ -1,5 +1,6 @@
 package com.aleksey_kuvshinov.calculator;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.SwitchCompat;
